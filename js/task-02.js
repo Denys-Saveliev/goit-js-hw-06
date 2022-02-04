47dd25251633a60603cd13cf48c7fb5278e36bfb
Denys-Saveliev <denys.saveliev@ukr.net>
2022-02-04 14:06:24 +0200

    changing task-2.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listRef = document.querySelector('#ingredients');
+
+const ingredientRef = ingredients.forEach(ingredient => {
+   const itemsRef = document.createElement('li');
+   itemsRef.textContent = ingredient;
+   itemsRef.classList.add('item');
+   console.log(itemsRef);
+   listRef.appendChild(itemsRef);
+});
