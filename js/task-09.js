6d65bc89cb292e9224c4febc11c3f3963e4e3daa
Denys-Saveliev <denys.saveliev@ukr.net>
2022-02-06 21:42:39 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const spanNameColor = document.querySelector('.color');
+const btnPushToChangeColor = document.querySelector('.change-color');
+
+btnPushToChangeColor.addEventListener('click', onBtnPushChangeColor);
+
+function onBtnPushChangeColor(event) {
+   const newColor = getRandomHexColor();
+   document.body.style.backgroundColor = newColor;
+   spanNameColor.textContent = newColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
